31572c8213037f005c43274aaaf2b9ad804a0395
Jason Tran <jason.tran@duke.edu>
2023-02-04 12:52:56 -0500

    created unit test for y_online function

---
+++ test_on_line.py
@@ -0,0 +1,13 @@
+import pytest
+
+
+@pytest.mark.parametrize("coordinate1, coordinate2, x, y",
+[((0, 0), (1, 1), 0.5, 0.5),
+((0, 0), (1, 2), 0.5, 1)
+])
+
+
+def test_y_online(coordinate1, coordinate2, x, y):
+    from on_line import y_online
+    answer = y_online(coordinate1, coordinate2, x)
+    assert answer == y
